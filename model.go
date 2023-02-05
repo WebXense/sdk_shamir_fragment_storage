91dacf390bf34c85264edff1521c970d43f28200
Nelson Lai <nelsonlai9503@gmail.com>
2023-02-05 16:36:19 +0800

    update request models

--- model.go
+++ model.go
@@ -10,18 +10,22 @@ type CreateFragmentRequest struct {
 	Key    string `json:"key" binding:"required"`
 	Weight uint8  `json:"weight" binding:"required"`
 	Value  string `json:"value" binding:"required"`
+	Secret string `json:"secret" binding:"required"`
 }
 
 type DeleteFragmentRequest struct {
-	Key string `json:"key" binding:"required"`
+	Key    string `json:"key" binding:"required"`
+	Secret string `json:"secret" binding:"required"`
 }
 
 type GetFragmentRequest struct {
-	Key string `form:"key" binding:"required"`
+	Key    string `form:"key" binding:"required"`
+	Secret string `form:"secret" binding:"required"`
 }
 
 type UpdateFragmentRequest struct {
 	Key    string `json:"key" binding:"required"`
 	Weight uint8  `json:"weight" binding:"required"`
 	Value  string `json:"value" binding:"required"`
+	Secret string `json:"secret" binding:"required"`
 }
